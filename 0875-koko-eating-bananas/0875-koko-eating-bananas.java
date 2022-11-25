b70e6134e456f8a1b2b29e8aaf1841a7e28d66c6
vardhan barishetti <67987365+b-vardhan21@users.noreply.github.com>
2022-11-25 07:07:03 -0500

    Time: 47 ms (40.85%), Space: 54.1 MB (31.78%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int left = 1;
+        int right = 1000000000;
+        //int speed=Integer.MAX_VALUE;
+        while(left <= right){
+            int k = left + (right - left)/2;
+            
+            if(CanEatInTime(piles, k , h)){
+                //speed = k;
+                right = k - 1;
+            }
+            else{
+                left = k + 1;
+            }
+            
+        }
+        
+        return left;
+    }
+    
+    boolean CanEatInTime(int[] piles, int k, int h){
+        long hours = 0;
+        for(int pile : piles){
+            hours += (int)Math.ceil(pile*1.0/k);
+            //if(pile % k!=0) hours++;
+        }
+        
+        return hours <= h;
+    }
+}
